07d265d9dfefe296f654907ad3a6e9cd90b1ab2d
Ziqiang Wei <weiz@janelia.hhmi.org>
2016-05-19 16:14:49 -0400

    recompute half EV time and half Active time

--- Codes/Data_Analysis/FAEV_v0_1.m
+++ Codes/Data_Analysis/FAEV_v0_1.m
@@ -2,7 +2,8 @@
 % 4.  Explained variance: Factor analysis -- corrected loading matrix --
 % sigmoid fit
 % 
-%
+% computing half EV time
+% computing half active time
 % 
 % -------------------------------------------------------------------------
 % Ziqiang Wei
@@ -15,14 +16,15 @@ function FAEV_v0_1(nFile)
     setDir;    
     fileName          = fileNames{nFile}; %#ok<USENS>    
     mnx               = [];
-    load([tempDatDir, fileName, '.mat'], 'mnx', 'tracks', 'side', 'leafOrder', 'slicedIndex'); 
+    load([tempDatDir, fileName, '.mat'], 'mnx', 'tracks', 'side', 'leafOrder', 'slicedIndex', 'activeNeuronMat'); 
     load([tempDatDir, 'EV_' fileName, '.mat'], 'EVMat')
     
-    numTime           = size(EVMat, 2); %#ok<USENS>
+    numTime           = size(EVMat, 2); %#ok<*NODEF>
     numNeuron         = size(EVMat, 1);
-    meantracks        = squeeze(mean(tracks, 2)); %#ok<NODEF>
     halfEVTime        = nan(numNeuron, 1);
     RSquare           = zeros(numNeuron, 1);
+    halfActTime       = nan(numNeuron, 1);
+    validFitIndex     = false(numNeuron, 1);
     
     m                 = ceil(sqrt(numNeuron));
     
@@ -30,6 +32,8 @@ function FAEV_v0_1(nFile)
     neuronName        = neuronName(leafOrder);
     
     figure;
+    timeBin           = 15;
+    activeThres       = 0.65;
     
     for nNeuron       = 1:numNeuron
         subplot(m, m, nNeuron)
@@ -41,31 +45,64 @@ function FAEV_v0_1(nFile)
         end
         
         timeIndex = ~isnan(EVMat(nNeuron, :));
-        if sum(timeIndex) > 10
-            [fitParams, fitResult] = sigm_fit(find(timeIndex)/60, EVMat(nNeuron, timeIndex)');
-            RSquare(nNeuron)         = 1 - mean((EVMat(nNeuron, timeIndex)' - fitResult.ypred).^2)./var(EVMat(nNeuron, timeIndex)');
-            halfEVTime(nNeuron)      = fitParams(3);
+        actCurrNeuron   = activeNeuronMat(nNeuron, :);
+        firstActiveTime = find(smooth(double(actCurrNeuron), timeBin) > activeThres, 1, 'first');
+        if ~isempty(firstActiveTime)
+            removeIndex     = find(actCurrNeuron==0);
+            removeIndex(removeIndex<firstActiveTime) = [];
+            removeTimeIndex = false(1, numTime);
+            removeTimeIndex(removeIndex) = true;
+            timeIndex = timeIndex & ~ removeTimeIndex;
+        else
+            removeTimeIndex = false(1, numTime);
         end
+ 
+        mdl = fitglm(find(~removeTimeIndex)/60, activeNeuronMat(nNeuron, ~removeTimeIndex), 'linear', 'Distribution', 'binomial');
+        beta = mdl.Coefficients.Estimate;
+        halfActTime(nNeuron) = -beta(1)/beta(2);
+        if (mean(activeNeuronMat(nNeuron, 1:ceil(numTime*.5))) > 0.80 ||  mean(activeNeuronMat(nNeuron, :)) > 0.80) ...
+                && (halfActTime(nNeuron)<0 || halfActTime(nNeuron)>numTime/60)
+            halfActTime(nNeuron) = 0;
+        end       
+        
+                
+        if sum(timeIndex) > 10
+            paddingLength            = 20;
+            zerosPadding             = zeros(1, paddingLength+1);
+            init_params              = [0, quantile(EVMat(nNeuron, timeIndex), 0.95), halfActTime(nNeuron), 1];
+            [fitParams, fitResult]   = sigm_fit([(-paddingLength:0)/60, find(timeIndex)/60], [zerosPadding, EVMat(nNeuron, timeIndex)], [0, nan, nan, nan], init_params, false);
+            ypred                    = fitResult.ypred(paddingLength+2: end);
+            ypredlowerCI             = fitResult.ypredlowerCI(paddingLength+2:end);
+            ypredupperCI             = fitResult.ypredupperCI(paddingLength+2:end);
+            RSquare(nNeuron)         = 1 - mean((EVMat(nNeuron, timeIndex)' - ypred).^2)./var(EVMat(nNeuron, timeIndex)'); %#ok<UDIM>
+            halfEVTime(nNeuron)      = fitParams(3);
+%             [~, fitResult]           = sigm_fit(find(timeIndex)/60, EVMat(nNeuron, timeIndex)');
+%             RSquare(nNeuron)         = 1 - mean((EVMat(nNeuron, timeIndex)' - fitResult.ypred).^2)./var(EVMat(nNeuron, timeIndex)');
+        end
+        
+
         
         hold on;
         plot(find(timeIndex)/60, EVMat(nNeuron, timeIndex), 'o', 'color', mColor);
-        if ~isnan(halfEVTime(nNeuron))
-            plot(find(timeIndex)/60, fitResult.ypred, '-', 'linewid', 2.0, 'Color', mColor);
-            plot(find(timeIndex)/60, fitResult.ypredlowerCI, '-', 'linewid', 0.5, 'Color', mColor); 
-            plot(find(timeIndex)/60, fitResult.ypredupperCI, '-', 'linewid', 0.5, 'Color', mColor); 
-        end
-        hold off;
-        
-        title(['#' num2str(neuronName(nNeuron)) ' R^2=' num2str(RSquare(nNeuron))])
-        
-        if ~isempty(mnx)
-            if mnx(nNeuron)
-                title(['#' num2str(neuronName(nNeuron)) ' MNX+; R^2=' num2str(RSquare(nNeuron))])
-            else
-                title(['#' num2str(neuronName(nNeuron)) ' MNX-; R^2=' num2str(RSquare(nNeuron))])
+        plot((1:numTime)/60, activeNeuronMat(nNeuron, :), '.k')
+        if ~isnan(halfEVTime(nNeuron)) && sum(activeNeuronMat(nNeuron, ~removeTimeIndex))>numTime*0.1
+            validFitIndex(nNeuron) = true;
+            plot(find(timeIndex)/60, ypred, '-', 'linewid', 2.0, 'Color', mColor);
+            plot(find(timeIndex)/60, ypredlowerCI, '-', 'linewid', 0.5, 'Color', mColor); 
+            plot(find(timeIndex)/60, ypredupperCI, '-', 'linewid', 0.5, 'Color', mColor); 
+            plot(find(~removeTimeIndex)/60, mdl.Fitted.Probability, '-k')
+            
+            title(['#' num2str(neuronName(nNeuron)) ' R^2=' num2str(RSquare(nNeuron))])
+            if ~isempty(mnx)
+                if mnx(nNeuron)
+                    title({['#' num2str(neuronName(nNeuron)) ' MNX+; R^2=' num2str(RSquare(nNeuron), '%.2f')]; ['halfEV:' num2str(halfEVTime(nNeuron), '%.2f') 'halfAct:' num2str(halfActTime(nNeuron), '%.2f')]})
+                else
+                    title({['#' num2str(neuronName(nNeuron)) ' MNX-; R^2=' num2str(RSquare(nNeuron), '%.2f')]; ['halfEV:' num2str(halfEVTime(nNeuron), '%.2f') 'halfAct:' num2str(halfActTime(nNeuron), '%.2f')]})
+                end
             end
         end
         
+        hold off;
         ylim([0 1])
         xlim([0 numTime/60])    
         xlabel('Time (hour)')
@@ -73,68 +110,6 @@ function FAEV_v0_1(nFile)
         box off
     end
     
-    setPrint(8*m, 6*m, [plotDir, 'SigFitEV_', fileName], 'pdf');
-    
-    RSquareThres = 0.9;
-    
-    figure;
-    xTrack       = meantracks(:, 1);
-    yTrack       = meantracks(:, 2);
-    zTrack       = meantracks(:, 3);
-    
-    halfEVTime(halfEVTime<0) = 0;
-    figure;
-    hold on
-    plot(xTrack(RSquare<RSquareThres), yTrack(RSquare<RSquareThres), 'o', 'MarkerFaceColor', [0.5 0.5 0.5], 'MarkerEdgeColor', [0.5 0.5 0.5] );
-%     scatter(xTrack(RSquare>=RSquareThres), yTrack(RSquare>=RSquareThres), [], halfEVTime(RSquare>=RSquareThres), 'fill')
-    
-    if isempty(mnx)
-        scatter(xTrack(RSquare>=RSquareThres), yTrack(RSquare>=RSquareThres), [], halfEVTime(RSquare>=RSquareThres), 'fill')
-    else
-        if sum(RSquare>=RSquareThres & mnx)>0
-            scatter(xTrack(RSquare>=RSquareThres & mnx), yTrack(RSquare>=RSquareThres & mnx), [], halfEVTime(RSquare>=RSquareThres & mnx), 'o', 'fill')
-        end
-        hold on
-        if sum(RSquare>=RSquareThres & ~mnx)>0
-            scatter(xTrack(RSquare>=RSquareThres & ~mnx), yTrack(RSquare>=RSquareThres & ~mnx), [], halfEVTime(RSquare>=RSquareThres & ~mnx), 's', 'fill')
-        end
-        hold off
-    end
-    hold off
-    
-    xlabel('x-axis')
-    ylabel('y-axis')
-    box off
-    colorbar
-        
-    setPrint(8, 6, [plotDir, 'HalfEVTimeXY_', fileName], 'pdf');
-    
-    figure;
-    hold on
-    plot(xTrack(RSquare<RSquareThres), zTrack(RSquare<RSquareThres), 'o', 'MarkerFaceColor', [0.5 0.5 0.5], 'MarkerEdgeColor', [0.5 0.5 0.5] );
-%     scatter(xTrack(RSquare>=RSquareThres), zTrack(RSquare>=RSquareThres), [], halfEVTime(RSquare>=RSquareThres), 'fill')
-    
-    if isempty(mnx)
-        scatter(xTrack(RSquare>=RSquareThres), zTrack(RSquare>=RSquareThres), [], halfEVTime(RSquare>=RSquareThres), 'fill')
-    else
-        if sum(RSquare>=RSquareThres & mnx)>0
-            scatter(xTrack(RSquare>=RSquareThres & mnx), zTrack(RSquare>=RSquareThres & mnx), [], halfEVTime(RSquare>=RSquareThres & mnx), 'o', 'fill')
-        end
-        hold on
-        if sum(RSquare>=RSquareThres & ~mnx)>0
-            scatter(xTrack(RSquare>=RSquareThres & ~mnx), zTrack(RSquare>=RSquareThres & ~mnx), [], halfEVTime(RSquare>=RSquareThres & ~mnx), 's', 'fill')
-        end
-        hold off
-    end
-    hold off
-    
-    xlabel('x-axis')
-    ylabel('z-axis')
-    box off
-    colorbar
-        
-    setPrint(8, 6, [plotDir, 'HalfEVTimeXZ_', fileName], 'pdf');
-    
-    close all
-    save([tempDatDir, 'EV_' fileName, '.mat'], 'halfEVTime', 'RSquare', '-append');
+    setPrint(8*m, 6*m, [plotDir, 'SigFitEVActiveNeuron_', fileName], 'pdf');
+    save([tempDatDir, 'EV_' fileName, '.mat'], 'halfActTime', 'halfEVTime', 'RSquare', 'validFitIndex', '-append');
 end
