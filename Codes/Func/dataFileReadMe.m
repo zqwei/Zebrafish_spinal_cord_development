cb70df4bb8f768bae6d1df0eb847df8eaba40c31
Ziqiang Wei <weiz@janelia.hhmi.org>
2017-02-24 22:36:19 -0500

    update for new datasets and run for ks 0.05 without global filter

--- Codes/Func/dataFileReadMe.m
+++ Codes/Func/dataFileReadMe.m
@@ -11,16 +11,16 @@
 % * 2 a right-patterned neuron
 
 %% Name List of data files
-fileDirNames{1} = '../../Data/20140818';
-fileDirNames{2} = '../../Data/20141006';
-fileDirNames{3} = '../../Data/20150410';
+% fileDirNames{1} = '../../Data/20140818';
+% fileDirNames{2} = '../../Data/20141006';
+% fileDirNames{3} = '../../Data/20150410';
+fileDirNames{1} = '../../Data/20140818_curated';
+fileDirNames{2} = '../../Data/20141006_curated';
+fileDirNames{3} = '../../Data/20150410_curated';
 fileDirNames{4} = '../../Data/20150417';
 fileDirNames{5} = '../../Data/20160312';
 fileDirNames{6} = '../../Data/20160328';
-fileDirNames{7} = '../../Data/20140818_curated';
-fileDirNames{8} = '../../Data/20141006_curated';
-fileDirNames{9} = '../../Data/20150410_curated';
-fileDirNames{10} = '../../Data/20161004';
-fileDirNames{11} = '../../Data/20161028';
-fileDirNames{12} = '../../Data/20170111';
-fileDirNames{13} = '../../Data/20170112';
+fileDirNames{7} = '../../Data/20161004';
+fileDirNames{8} = '../../Data/20161028';
+fileDirNames{9} = '../../Data/20170111';
+fileDirNames{10} = '../../Data/20170112';
