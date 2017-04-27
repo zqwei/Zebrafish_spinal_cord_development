76143aea518a6971852ddbe4b3f1b2d198971c40
Ziqiang Wei <weiz@janelia.hhmi.org>
2017-04-27 13:30:56 -0400

    add path for network dynamics analysis

--- Codes/Func/setDir.m
+++ Codes/Func/setDir.m
@@ -10,27 +10,9 @@ end
 %%%%% Add command for warning off (especially for file loads)
 warning('off','all')
 
-%%%%% Add Path for Plots
-% addpath('../../../../Empirical_Data_Analysis_Code/Plots/');
-% addpath('../../../Empirical_Data_Analysis_Code/GraphViz/');
-
-%%%%% Add Path for LDSI Fit
-addpath('../../../../Empirical_Data_Analysis_Code/LDSI/Release_LDSI_v3/');
-% addpath('../../../../Empirical_Data_Analysis_Code/x3d_version3g/');
-
-
-
-% dirImageData      = [fileDirName, '/Data/'];
-% TempDat           = [fileDirName, '/Fitted_result/'];
 
 plotDir             = '../../Plots/';
-
-%if ~exist(plotDir, 'dir')
-%    mkdir(plotDir)
-%end
+plotNetDir          = '../../NetworkDynamicsPlots/';
 
 tempDatDir          = '../../TempDat/';
-
-%if ~exist(tempDatDir, 'dir')
-%    mkdir(tempDatDir)
-%end
+tempDatNetDir       = '../../NetworkDynamicsTempDat/';
