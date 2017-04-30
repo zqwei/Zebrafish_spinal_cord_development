cb31499eb1859b37203035f817da26222b281b14
Ziqiang Wei <weiz@janelia.hhmi.org>
2017-04-30 17:48:33 -0400

    add plot for neural activity in small factors

--- Codes/Single_neuron_network_tracks/NeuralActivityFactorTime_v0_4.m
+++ Codes/Single_neuron_network_tracks/NeuralActivityFactorTime_v0_4.m
@@ -15,48 +15,37 @@
 function NeuralActivityFactorTime_v0_4(nFile)
     addpath('../Func');
     setDir;    
-    fileName   = fileNames{nFile};   %#ok<*USENS>
-    load([tempDatDir, fileName, '.mat'], 'dff', 'timePoints', 'new_x', 'new_y', 'new_z')
-    load([tempDatNetDir, 'LONOLoading_' fileName, '_v_0_1.mat'], 'new_activeNeuronMat') 
+    fileName      = fileNames{nFile};   %#ok<*USENS>
+    load([tempDatDir, fileName, '.mat'], 'dff', 'timePoints')
     load([tempDatNetDir, 'LONOLoading_' fileName, '_v_0_2.mat'], 'preLMat', 'preLMatIndex', 'preLMatTime') 
-    numTime           = size(new_activeNeuronMat, 2);
-    numNeuron         = size(new_activeNeuronMat, 1);
-    neuronFactorIndex = false(size(new_activeNeuronMat));
-    timeWin           = 40;
-    linew             = 0.5;
-    colorSet          = cbrewer('qual', 'Dark2',  numTime, 'cubic');
-
-    for nTime = 1:numTime
-        LMat  = preLMat(:, preLMatTime==nTime);
-        if ~isempty(LMat)
-            neuronFactorIndex(sum(LMat, 2)>0, nTime) = true;
-        end
-    end
-    
-    neuronTimeValue = double(neuronFactorIndex & new_activeNeuronMat);
-    for nNeuron     = 1:numNeuron
-        smooth_neuronTimeValue      = smooth([zeros(1, 11), neuronTimeValue(nNeuron, :)], 21);
-        neuronTimeValue(nNeuron, :) = smooth_neuronTimeValue(1:end-11) > 0.3;
-    end
-    
-    neuronTimeValue = neuronFactorIndex & new_activeNeuronMat & neuronTimeValue;
-    
-    
-    neuronTime = nan(numNeuron, 1);
-    neuronActTime = nan(numNeuron, 1);
-    
-    for nNeuron = 1:numNeuron
-        if sum(neuronTimeValue(nNeuron, :)) > 0
-            neuronTime(nNeuron) = find(neuronTimeValue(nNeuron, :), 1, 'first');
-            neuronActTime(nNeuron) = mean(new_activeNeuronMat(nNeuron, max(1, (neuronTime(nNeuron)-timeWin):neuronTime(nNeuron))));
-        end
-    end
+    numTime       = length(timePoints);
+    numFactor     = max(preLMatIndex);
+    preLMatNeuron = sum(preLMat);
+    timeWin       = 40;
+    mColor        = cbrewer('qual', 'Dark2',  numFactor, 'cubic');
+    nStep         = 10;
     
     figure;
-    [fout, xout] = ksdensity(neuronActTime(~isnan(neuronActTime)), 0:0.02:1);
-    plot(xout, fout, 'linewid', 2)
-    xlim([0 1])
-    box off
-    setPrint(8, 6, [plotNetDir 'SingleNeuronActLevelFactorTime_' fileName])
-    
+    totNeuron     = 0;
+    for nFactor   = 1:numFactor
+        timeInd   = preLMatTime(:, preLMatNeuron<4 & preLMatIndex == nFactor);
+        zeroTime  = min(timeInd);
+        endTime   = min(max(timeInd), zeroTime+timeWin);
+        LMat      = preLMat(:, preLMatNeuron<4 & preLMatIndex == nFactor & preLMatTime<=endTime);
+        LMatInd   = sum(LMat, 2)>0;
+        zeroTime  = min(timeInd);
+        endTime   = min(max(timeInd), zeroTime+timeWin);
+        minTime   = max(zeroTime - timeWin, 1);
+        timeRange = timePoints(minTime)+1:timePoints(endTime);
+        timeMarks = timeRange - timePoints(zeroTime);
+        dffValue  = bsxfun(@plus, zscore(dff(LMatInd, timeRange), [], 2), (1:sum(LMatInd))'*nStep + totNeuron*nStep);
+        totNeuron = totNeuron + sum(LMatInd);
+        hold on
+        plot(timeMarks, dffValue, 'Color', mColor(nFactor,:), 'linewidth', 0.5);
+        axis off
+    end
+    gridxy(0, [], 'color', 'w', 'linestyle', '--')
+    xlim([-timePoints(timeWin) timePoints(timeWin)+10])
+    ylim([-1 (totNeuron+1)*nStep+1])
+    setPrint(20, 4*numFactor, [plotNetDir 'SingleNeuronDynamicsLocalCommunity_' fileName], 'pdf')
 end
