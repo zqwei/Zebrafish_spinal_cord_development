4a607b660004fa65c718dbe75c14041e92023862
Yinan Wan <wany@janelia.hhmi.org>
2017-05-03 19:38:53 -0400

    curated data for 20170315

--- Codes/Data_Analysis/Data_Analysis_List_Yinan.m
+++ Codes/Data_Analysis/Data_Analysis_List_Yinan.m
@@ -1,7 +1,7 @@
 % % function Data_Analysis_List_all
 numFile = 19;
 
-for nFile = 1:numFile              
+for nFile = 20              
     disp(['Generate the orginal data file for dataset #' num2str(nFile)]);   
     disp('==================================');
     disp('Analysis #1 -- clustering analysis');   
--- Codes/Func/dataFileReadMe.m
+++ Codes/Func/dataFileReadMe.m
@@ -32,5 +32,6 @@ fileDirNames{16} = '../../Data/20170216';
 fileDirNames{17} = '../../Data/20170315';
 fileDirNames{18} = '../../Data/20170323';
 fileDirNames{19} = '../../Data/20170412';
+fileDirNames{20} = '../../Data/20170315_ext';
 
 
