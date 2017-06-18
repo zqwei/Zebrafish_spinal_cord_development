5d70d2f9d3e2dd0824fe7dbb1c92537289320554
Ziqiang Wei <weiz@janelia.hhmi.org>
2017-06-18 09:59:25 -0400

    update rule for dropped data in MO sigmoid fit

--- Codes/Data_Analysis/FACluster_v1_1_3.m
+++ Codes/Data_Analysis/FACluster_v1_1_3.m
@@ -57,7 +57,7 @@ function FACluster_v1_1_3(nFile)
     % for nfile = 17: removeStart  = 180; fix_fit_max = 0.95;
     % for nfile = 18; removeStart  = 120;
     % for nfile = 19; removeStart  = 150; fix_fit_max = 0.95;
-    removeStart  = 180;
+    if nFile >=17; removeStart  = 180; end
     init_params              = [0, max(fracNeuron), find(fracNeuron>0.5, 1, 'first')/60, 1];
     [fitParams, fitResult]   = sigm_fit((1:removeStart)/60, fracNeuron(1:removeStart), [0, 0.95, nan, nan], init_params, false);
     upK                      = fitResult.paramCI(end, 1);
