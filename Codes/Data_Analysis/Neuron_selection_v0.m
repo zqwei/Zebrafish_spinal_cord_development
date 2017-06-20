5600e0e64e67d3f54c9995daedff57184078c48d
Ziqiang Wei <weiz@janelia.hhmi.org>
2017-06-20 11:38:33 -0400

    compute activation mat using multiple percentile baselines (window size = 21, 41, 61 and p = 20%) as well as SG fit

--- Codes/Data_Analysis/Neuron_selection_v0.m
+++ Codes/Data_Analysis/Neuron_selection_v0.m
@@ -27,24 +27,18 @@ function Neuron_selection_v0(nFile)
     dirImageData  = [fileDirName '/'];
     load ([dirImageData 'profile.mat'])
 
-    % Savitzky-Golay filtering
-    sg_dff        = profile_all;
-    numOrder      = 9;
-    lenWindow     = 511;
-    baseline      = sgolayfilt(sg_dff, numOrder, lenWindow, [], 2);
-    sg_dff        = bsxfun(@rdivide,(sg_dff - baseline), mean(baseline, 2));
-    nCells        = size(sg_dff, 1);
-
     % percentile window
     dff           = profile_all;
     rawf          = profile_all;
-    w             = 61; % baselineWindowSize
+    w             = 21; % baselineWindowSize
     p             = 20; % baselinePrc
+    background    = 90;
     baseline      = dff;
+    nCells        = size(dff, 1);
     for nNeuron   = 1:nCells
         baseline(nNeuron, :) = running_percentile(dff(nNeuron, :), w, p);
     end
-    dff           = bsxfun(@rdivide,(dff - baseline), mean(baseline, 2));
+    dff           = bsxfun(@rdivide,(dff - baseline), (mean(baseline, 2)-background));
 
     tracks        = tracks_smoothed;
 
@@ -67,28 +61,11 @@ function Neuron_selection_v0(nFile)
     baseline      = baseline(slicedIndex, timeStep:timeEnd+timeStep);
     rawf          = rawf(slicedIndex, timeStep:timeEnd+timeStep);
     dff           = dff(slicedIndex, timeStep:timeEnd+timeStep);
-    sg_dff        = sg_dff(slicedIndex, timeStep:timeEnd+timeStep);
     tracks        = tracks(slicedIndex, timeStep:timeEnd+timeStep, :);
     side          = side(slicedIndex); %#ok<NODEF>
 
 
     timePoints    = 0:240:timeEnd-timeStep;     %#ok<NASGU>
-
-
-% %     logDetCorrAllDFF = zeros(length(timePoints),1);
-% %     for nTime           = 1:length(timePoints)
-% %         slicedDFF       = dff(:, timePoints(nTime)+1:timePoints(nTime)+1200);
-% %         logDetCorrAllDFF(nTime) = log(det(corr(slicedDFF')));
-% %     end
-% %     figure;
-% %     plot(timePoints/4/60, logDetCorrAllDFF, '-', 'linewid', 2)
-% %     xlim([0 timePoints(end)/4/60]);
-% %     xlabel('Time (min)')
-% %     ylabel('log|Corr|')
-% %     title('Colinearity index')
-% %     box off
-% %     setPrint(8, 6, [plotDir 'LogDetCorrIndex_' fileName], 'pdf')
-
 
     slicedDFF       = dff(:, end - timeStep*10:end);
     distNeurons     = pdist(slicedDFF, 'correlation');
@@ -103,68 +80,30 @@ function Neuron_selection_v0(nFile)
     numNeuron     = sum(slicedIndex);
     m             = ceil(sqrt(length(timePoints)/10+1));
 
-% %     figure;
-% %     for nPlot           = 1:10:length(timePoints)
-% %         subplot(m, m, (nPlot-1)/10+1)
-% %         hold on
-% %         slicedDFF       = dff(:, timePoints(nPlot)+1:timePoints(nPlot)+1200);
-% %         imagesc(corr(slicedDFF(leafOrder,:)'),[-1 1]);
-% %         plot([1 numNeuron], [sideSplitter sideSplitter], '--w')
-% %         plot([sideSplitter sideSplitter], [1 numNeuron], '--w')
-% %         xlim([1 numNeuron])
-% %         ylim([1 numNeuron])
-% %         xlabel('Neuronal index')
-% %         ylabel('Neuronal index')
-% %         title([num2str(nPlot) 'min']);
-% %         box off;
-% %         hold off
-% %     end
-% %     setPrint(m*8, m*6, [plotDir, 'CorrMat_', fileName], 'pdf');
-% %
-% %
-% %     figure;
-% %     for nPlot           = 1:10:length(timePoints)
-% %         subplot(m, m, (nPlot-1)/10+1)
-% %         slicedDFF       = dff(:,timePoints(nPlot)+1:timePoints(nPlot)+1200);
-% %         distNeurons     = pdist(slicedDFF, 'correlation');
-% %         linkNeurons     = linkage(slicedDFF,'single','correlation');
-% %         leafOrderLocal  = optimalleaforder(linkNeurons, distNeurons);
-% %         imagesc(corr(slicedDFF(leafOrderLocal,:)'),[-1 1]);
-% %         xlim([1 numNeuron])
-% %         ylim([1 numNeuron])
-% %         xlabel('Neuronal index')
-% %         ylabel('Neuronal index')
-% %         title([num2str(nPlot) 'min']);
-% %         box off;
-% %         axis xy
-% %     end
-% %     setPrint(m*8, m*6, [plotDir, 'CorrMatLocal_', fileName], 'pdf');
-
 
     baseline      = baseline(leafOrder, :); %#ok<NASGU>
     rawf          = rawf(leafOrder, :); %#ok<NASGU>
     dff           = dff(leafOrder, :); %#ok<NASGU>
-    sg_dff        = sg_dff(leafOrder, :); %#ok<NASGU>
     tracks        = tracks(leafOrder, :, :); %#ok<NASGU>
     side          = side(leafOrder, :);  %#ok<NASGU>
 
     activeNeuronMat   = false(size(dff, 1), length(timePoints));
-    activeNeuronSGMat = false(size(dff, 1), length(timePoints));
 
     for nNeuron    = 1:size(dff, 1)
         for nTime  = 1:length(timePoints)
-            % Percentile activeNeuronMat
             slicedDFF           = dff(nNeuron, timePoints(nTime)+1:timePoints(nTime)+1200); %1200
             slicedDFF           = (slicedDFF - mean(slicedDFF))/std(slicedDFF);
             activeNeuronMat(nNeuron, nTime) = kstest2(-slicedDFF(slicedDFF<0), slicedDFF(slicedDFF>0), 'alpha', 0.01) && (skewness(slicedDFF)>0);
-            % SG activeNeuronMat
-            slicedDFF           = sg_dff(nNeuron, timePoints(nTime)+1:timePoints(nTime)+1200); %1200
-            slicedDFF           = (slicedDFF - mean(slicedDFF))/std(slicedDFF);
-            activeNeuronSGMat(nNeuron, nTime) = kstest2(-slicedDFF(slicedDFF<0), slicedDFF(slicedDFF>0), 'alpha', 0.05) && (skewness(slicedDFF)>0);
         end
     end
 
-    activeNeuronMat = activeNeuronMat & activeNeuronSGMat;
+    refActiveNeuronMat = activeMatUseSGFit(rawf, 9, 511, timePoints, 0.05);
+    activeNeuronMat    = activeNeuronMat & refActiveNeuronMat;
+    
+    for w                  = [41 61]
+        refActiveNeuronMat = activeMatUsePercentile(rawf, w, p, background, timePoints, 0.01);
+        activeNeuronMat    = activeNeuronMat & refActiveNeuronMat;
+    end
 
     save([tempDatDir, fileName, '.mat'], 'dff', 'tracks', 'leafOrder', 'slicedIndex', 'side', 'timePoints', 'sideSplitter', 'activeNeuronMat');
     if exist('mnx', 'var')
@@ -173,7 +112,39 @@ function Neuron_selection_v0(nFile)
         save([tempDatDir, fileName, '.mat'], 'mnx', '-append')
     end
 
-%     close all
-%
-
 end
+
+function activeNeuronMat = activeMatUsePercentile(rawf, w, p, background, timePoints, alpha_value)
+    dff           = rawf;
+    baseline      = dff;
+    % percentile window
+    for nNeuron   = 1:size(dff, 1)
+        baseline(nNeuron, :) = running_percentile(dff(nNeuron, :), w, p);
+    end
+    dff           = bsxfun(@rdivide,(dff - baseline), (mean(baseline, 2)-background));
+    activeNeuronMat   = false(size(dff, 1), length(timePoints));
+    for nNeuron    = 1:size(dff, 1)
+        for nTime  = 1:length(timePoints)
+            slicedDFF           = dff(nNeuron, timePoints(nTime)+1:timePoints(nTime)+1200); %1200
+            slicedDFF           = (slicedDFF - mean(slicedDFF))/std(slicedDFF);
+            activeNeuronMat(nNeuron, nTime) = kstest2(-slicedDFF(slicedDFF<0), slicedDFF(slicedDFF>0), 'alpha', alpha_value) && (skewness(slicedDFF)>0);
+        end
+    end
+end
+
+
+function activeNeuronMat = activeMatUseSGFit(rawf, numOrder, lenWindow, timePoints, alpha_value)
+    dff           = rawf;
+    % Savitzky-Golay filtering
+    baseline      = sgolayfilt(dff, numOrder, lenWindow, [], 2);
+    dff           = bsxfun(@rdivide,(dff - baseline), mean(baseline, 2));
+    activeNeuronMat   = false(size(dff, 1), length(timePoints));
+    for nNeuron    = 1:size(dff, 1)
+        for nTime  = 1:length(timePoints)
+            slicedDFF           = dff(nNeuron, timePoints(nTime)+1:timePoints(nTime)+1200); %1200
+            slicedDFF           = (slicedDFF - mean(slicedDFF))/std(slicedDFF);
+            activeNeuronMat(nNeuron, nTime) = kstest2(-slicedDFF(slicedDFF<0), slicedDFF(slicedDFF>0), 'alpha', alpha_value) && (skewness(slicedDFF)>0);
+        end
+    end
+end
+
