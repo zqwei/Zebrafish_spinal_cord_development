c465d85d7c6db9f1e3452c13fbd6fe43375efca3
Yinan Wan <wany@janelia.hhmi.org>
2017-07-19 21:40:13 -0400

    use setprint

--- Codes/Figure_Codes/Figure_list.m
+++ Codes/Figure_Codes/Figure_list.m
@@ -1,8 +1,10 @@
 % figure 2_d
 control_datasets = [3, 4, 7, 12, 10, 11, 13, 15, 16];
 
+setDir;    
 h = figure('Position', [0, 0, 1500, 200]);
 hold on;
 for nFile = control_datasets
     Figure_2(h, nFile);
 end
+setPrint(8*totPlots, 6, [plotDir 'Figure_2d'], 'pdf')
