a5817517b9fca11a5da93c7768a2d0d328d20a79
Ziqiang Wei <weiz@janelia.hhmi.org>
2017-08-03 21:32:54 -0400

    add analysis of neural active time with neural initial factor size

---
+++ Codes/Single_neuron_network_tracks/NeuralActivityFactorTime_v0_5_2.m
@@ -0,0 +1,42 @@
+addpath('../Func');
+setDir;    
+
+fileToAnalysis = [3, 4, 7, 12, 10, 11, 13, 15, 16];
+
+neuronActTimeAll = [];
+neuronFASizeAll  = [];
+
+for nFile = fileToAnalysis
+    fileName   = fileNames{nFile};   %#ok<*USENS>
+    load([tempDatNetDir, 'NeuronActTime', fileName, '.mat'], 'neuronActTime', 'neuronTime', 'neuronFASize');
+    
+    neuronActTimeAll = [neuronActTimeAll; neuronActTime];
+    neuronFASizeAll  = [neuronFASizeAll; neuronFASize];
+    
+end
+
+
+neuronActTimeThres = 0.6;
+bins  = 1:10;
+figure;
+subplot(1, 3, 1)
+[fout, xout] = hist(neuronActTimeAll(~isnan(neuronActTimeAll)), 0:0.05:1);
+stairs(xout, fout, 'linewid', 2)
+xlim([0 1.1])
+xlabel('Peak fraction of activation period')
+ylabel('Number Neuron')
+box off
+
+subplot(1, 3, 2)
+[fout, xout] = hist(neuronFASizeAll(neuronActTimeAll>neuronActTimeThres), bins);
+stairs(xout, fout, 'linewid', 2)
+xlabel('Factor size')
+ylabel('Number Neuron')
+box off
+
+subplot(1, 3, 3)
+[fout, xout] = hist(neuronFASizeAll(neuronActTimeAll<=neuronActTimeThres), bins);
+stairs(xout, fout, 'linewid', 2)
+xlabel('Factor size')
+ylabel('Number Neuron')
+box off
