5632d740e2a6a35ea651d8c40c76afc0944bdd02
Ziqiang Wei <weiz@janelia.hhmi.org>
2017-08-04 18:09:20 -0400

    update parameters

--- Codes/Single_neuron_network_tracks/NeuralActivityFactorTime_v0_5_2.m
+++ Codes/Single_neuron_network_tracks/NeuralActivityFactorTime_v0_5_2.m
@@ -23,20 +23,21 @@ subplot(1, 3, 1)
 [fout, xout] = hist(neuronActTimeAll(~isnan(neuronActTimeAll)), 0:0.05:1);
 stairs(xout, fout, 'linewid', 2)
 xlim([0 1.1])
-xlabel('Peak fraction of activation period')
-ylabel('Number Neuron')
+xlabel('Peak fraction of activation period', 'fontsize', 24)
+ylabel('Number Neuron', 'fontsize', 24)
 box off
 
 subplot(1, 3, 2)
 [fout, xout] = hist(neuronFASizeAll(neuronActTimeAll>neuronActTimeThres), bins);
 stairs(xout, fout, 'linewid', 2)
-xlabel('Factor size')
-ylabel('Number Neuron')
+xlabel('Factor size', 'fontsize', 24)
+ylabel('Number Neuron', 'fontsize', 24)
+
 box off
 
 subplot(1, 3, 3)
 [fout, xout] = hist(neuronFASizeAll(neuronActTimeAll<=neuronActTimeThres), bins);
 stairs(xout, fout, 'linewid', 2)
-xlabel('Factor size')
-ylabel('Number Neuron')
+xlabel('Factor size', 'fontsize', 24)
+ylabel('Number Neuron', 'fontsize', 24)
 box off
