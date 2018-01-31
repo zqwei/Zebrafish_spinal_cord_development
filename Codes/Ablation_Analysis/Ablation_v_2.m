45428122200da48e99b54b938fd3e9594fe535d8
Yinan Wan <wany@janelia.hhmi.org>
2018-01-31 17:54:14 -0500

    add code to for activation test after ablation

--- Codes/Ablation_Analysis/Ablation_v_2.m
+++ Codes/Ablation_Analysis/Ablation_v_2.m
@@ -1,5 +1,5 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% 0.  evaluate change of number of active neurons and level of sync
+% 2.  evaluate change of number of active neurons and level of sync
 %
 % plot on dark background
 % 
@@ -11,12 +11,15 @@
 addpath('../Func');
 setDir;
 
+pThres = 0.05;
 nFileList = 25:2:58;
 
 fishListCutA = [1, 2, 3, 4, 7]; % anterior cut
-fishListCutM = [5, 8, 9, 10, 12]; % middle cut
+fishListCutM = [8, 9, 10, 12]; % middle cut
 fishListCutP = [13, 15, 16, 17]; % posterior cut
 
+useTagAfter = 1; %0: use all neurons that are active before, 1: use neurons that are active before & after
+tagExt = [];
 
 fishList = [fishListCutA, fishListCutM, fishListCutP]; % anterior cut
 correlationAP = zeros(numel(fishList), 2, 2);
@@ -24,6 +27,11 @@ fracActNeuron = zeros(numel(fishList), 4, 2);
 avgCorr = zeros(numel(fishList), 4, 2);
 numCells = zeros(numel(fishList), 4, 2);
 numActCells = zeros(numel(fishList), 4, 2);
+percOverlap = zeros(numel(fishList), 1);
+
+if useTagAfter
+    tagExt = '_useTagAfter';
+end
 for i = 1:numel(fishList)
     nFish = fishList(i);
     for nExp = 1:2
@@ -33,19 +41,22 @@ for i = 1:numel(fishList)
         
         dirImageData  = [fileDirName '/'];
         load([dirImageData, 'profile.mat'], 'segAblation');
-        load ([tempDatDir, fileName, '.mat'], 'dff', 'activeNeuronMat', 'new_x', 'new_y', 'new_z');
+        load ([tempDatDir, fileName, '.mat'], 'dff', 'activeNeuronMat', 'new_x', 'new_y', 'new_z', 'leafOrder');
         
         
-        activeThres = floor(size(activeNeuronMat, 2)/4); %1
-        activeTag = sum(activeNeuronMat, 2)> activeThres;
+        if nExp == 1
+            activeTag = activeNeuronMat;
+        else
+           load ([tempDatDir, fileName, '.mat'], 'activeTagBefore', 'activeTagAfter');
+           activeTag = activeTagBefore;
+           if useTagAfter
+               activeTag = activeTag & activeTagAfter;
+           end
+        end
         ind{1} = new_x<segAblation(1) & new_y<0;
         ind{2} = new_x<segAblation(1) & new_y>0;
         ind{3} = new_x>segAblation(2) & new_y<0;
         ind{4} = new_x>segAblation(2) & new_y>0;
-        
-        %         if ismember(nFish, fishListCutP)
-        %             ind = ind([3, 4, 1, 2]);
-        %         end
         
         corMatrix = corr(dff');
         % 1. Correlaiton between average traces A and P to cutting site
@@ -78,53 +89,61 @@ correlationAPCombined = squeeze(nanmean(correlationAP, 2));
 
 figure,
 hold on
-plot([1, 2], correlationAPCombined', 'ow');
-plot([1, 2], correlationAPCombined', 'w');
+plot([1, 2], correlationAPCombined', 'ok');
+plot([1, 2], correlationAPCombined', 'k');
 xlim([0.5, 2.5]);
 scatter([1, 2], mean(correlationAPCombined, 1), 'or', 'linew', 3);
 plot([1, 2], mean(correlationAPCombined, 1), 'r', 'linew', 3);
+[h, p] = ttest(correlationAPCombined(: , 1), correlationAPCombined(: , 2));
 ylim([-0.2, 1]);
 set(gca, 'Xtick', [1, 2], 'Xticklabel', {'before', 'after'});
+if h
+    text(1.5, max(correlationAPCombined(:))*1.2, '*');
+end
 hold off
 title('correlation between A and P')
-whitebg('black');
-set(gcf, 'Color', 'black');
-set(gcf, 'InvertHardCopy', 'off', 'PaperPositionMode', 'Auto');
-print([plotDir '/CorrelationAP.pdf'], '-dpdf', '-r0');
+setPrint(8, 6, [plotDir '/CorrelationAP' tagExt], 'pdf');
+
 
 fishListType = {1:numel(fishListCutA), numel(fishListCutA)+1:numel(fishListCutA)+numel(fishListCutM), numel(fishListCutA)+numel(fishListCutM)+1:numel(fishList); ...
     'Cut A', 'Cut M', 'Cut P'};
-figure('Position', [0, 0, 400*3, 300*2]);
+figure('Position', [0, 0, 400*3, 300]);
 for nExpType = 1:size(fishListType, 2)
     currfishList = fishListType{1, nExpType};
     fracActNeuronCombined = [fracActNeuron(currfishList, [1, 3], :); fracActNeuron(currfishList, [2, 4], :)];
     avgCorrCombined = [avgCorr(currfishList, [1, 3], :); avgCorr(currfishList, [2, 4], :)];
-    subplot(2, size(fishListType, 2), nExpType);
-    hold on,
-    for nSec = 1:2
-        xSeq = repmat([2*nSec-1; 2*nSec], 1, numel(currfishList)*2);
-        ySeq = squeeze(fracActNeuronCombined(:, nSec, :))';
-        scatter(xSeq(:), ySeq(:), 'ow');
-        plot(xSeq, ySeq, 'w');
-        scatter([2*nSec-1, 2*nSec], nanmean(squeeze(fracActNeuronCombined(:, nSec, :)))', 'or', 'linew', 3);
-        plot([2*nSec-1, 2*nSec], nanmean(squeeze(fracActNeuronCombined(:, nSec, :)))', 'r', 'linew', 3);
-    end
-    hold off
-    xlim([0, 5]);
-    ylim([0, 0.5]);
-    set(gca, 'Xtick', 1.5:2:5.5, 'XtickLabel', {'A', 'P'});
-    title(['fraction of active neurons - ' fishListType{2, nExpType}])
-    %     whitebg('black');
-    %     set(gcf, 'Color', 'black');
-    %     set(gcf, 'InvertHardCopy', 'off', 'PaperPositionMode', 'Auto');
+%     subplot(2, size(fishListType, 2), nExpType);
+%     hold on,
+%     for nSec = 1:2
+%         xSeq = repmat([2*nSec-1; 2*nSec], 1, numel(currfishList)*2);
+%         ySeq = squeeze(fracActNeuronCombined(:, nSec, :))';
+%         scatter(xSeq(:), ySeq(:), 'ok');
+%         plot(xSeq, ySeq, 'k');
+%         h = ttest(ySeq(1, :), ySeq(2, :), 'alpha', p);
+%         if ~isnan(h) && h
+%             text(2*nSec-0.5, max(ySeq(:))*1.2, '*');
+%         end
+%         scatter([2*nSec-1, 2*nSec], nanmean(squeeze(fracActNeuronCombined(:, nSec, :)))', 'or', 'linew', 3);
+%         plot([2*nSec-1, 2*nSec], nanmean(squeeze(fracActNeuronCombined(:, nSec, :)))', 'r', 'linew', 3);
+%     end
+%     hold off
+%     xlim([0, 5]);
+%     ylim([0, 1]);
+%     set(gca, 'Xtick', 1.5:2:5.5, 'XtickLabel', {'A', 'P'});
+%     title(['fraction of active neurons - ' fishListType{2, nExpType}])
+
     
-    subplot(2, size(fishListType, 2), nExpType+size(fishListType, 2));
+    subplot(1, size(fishListType, 2), nExpType);
     hold on,
     for nSec = 1:2
         xSeq = repmat([2*nSec-1; 2*nSec], 1, numel(currfishList)*2);
         ySeq = squeeze(avgCorrCombined(:, nSec, :))';
-        scatter(xSeq(:), ySeq(:), 'ow');
-        plot(xSeq, ySeq, 'w');
+        scatter(xSeq(:), ySeq(:), 'ok');
+        plot(xSeq, ySeq, 'k');
+        [h, p] = ttest(ySeq(1, :), ySeq(2, :));
+        if p < pThres
+            text(2*nSec-0.5, max(ySeq(:))*1.2, '*');
+        end
         scatter([2*nSec-1, 2*nSec], nanmean(squeeze(avgCorrCombined(:, nSec, :)))', 'or', 'linew', 3);
         plot([2*nSec-1, 2*nSec], nanmean(squeeze(avgCorrCombined(:, nSec, :)))', 'r', 'linew', 3);
     end
@@ -134,7 +153,8 @@ for nExpType = 1:size(fishListType, 2)
     set(gca, 'Xtick', 1.5:2:5.5, 'XtickLabel', {'A', 'P'});
     title(['level of synchronization - '  fishListType{2, nExpType}])
 end
-whitebg('black');
-set(gcf, 'Color', 'black');
-set(gcf, 'InvertHardCopy', 'off', 'PaperPositionMode', 'Auto');
-print([plotDir '/AblationTypeSummary.pdf'], '-dpdf', '-r0');
+
+
+setPrint(8*3, 6, [plotDir '/AblationTypeSummary' tagExt], 'pdf');
+
+close all
